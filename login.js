897f5b9de98671eb07cdbda9796ae6874887acdc
Steve <steve.mothmora@gmail.com>
2023-03-28 00:17:13 +0200

    Trying things with the "post" method

---
+++ login.js
@@ -0,0 +1,7 @@
+// File created to make tests concerning login routes | possibilities
+
+fetch("http://localhost:5678/api/users/login", {
+    method: "POST",
+    headers: {"Content-Type": ""},
+    body: chargeUtile
+})
