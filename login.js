8ea4c1afa7e5eef47fa8c8f9b097ca6b8eed8e2e
Steve <steve.mothmora@gmail.com>
2023-03-29 18:06:24 +0200

    testing the login functionnalitty with the user

--- login.js
+++ login.js
@@ -1,7 +1,25 @@
 // File created to make tests concerning login routes | possibilities
 
+const user = {
+    "email": "sophie.bluel@test.tld",
+    "password": "S0phie" 
+}
+
+const chargeUtile = JSON.stringify(user)
+
+
+
 fetch("http://localhost:5678/api/users/login", {
     method: "POST",
-    headers: {"Content-Type": ""},
+    headers: {
+        "Content-Type": "application/json"
+    },
     body: chargeUtile
 })
+.then(response => response.json())
+.then(data => {console.log(data)})
+
+
+
+// const btnLogin = document.querySelector("#btn-connect");
+// btnLogin.addEventListener("click", function() {
